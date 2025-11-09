9cd1550159c4da9ec8e9f48838cd2b89257d768b
Abhishek Kumar <amishra845438@gmail.com>
2025-11-09 22:33:40 +0530

    Initial frontend commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">PRIYANKA</div>
+          <div className="right">
+            <p>Cook With BABA, Bettiah, Bihar</p>
+            <p>Open: 02:00 PM - 02:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By PRIYANKA</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By BABA.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
